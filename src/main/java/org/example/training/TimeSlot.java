f17a0f4072015395ef6c9e3fcc567d42e2b154c2
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-06-30 15:52:04 +0100

    Refactor the ime slot in order to use a date time in preparation of work that deals with shifting hours that might cross midnight

--- src/main/java/org/example/training/TimeSlot.java
+++ src/main/java/org/example/training/TimeSlot.java
@@ -1,6 +1,22 @@
 package org.example.training;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
-record TimeSlot(LocalTime startTime, Duration duration) {}
+record TimeSlot(OffsetDateTime startTime, Duration duration) {
+
+    public TimeSlot(LocalTime startTime, Duration duration) {
+        this(
+            OffsetDateTime.of(
+                LocalDate.now(),
+                startTime,
+                ZoneOffset.UTC
+            ),
+            duration
+        );
+    }
+
+}
